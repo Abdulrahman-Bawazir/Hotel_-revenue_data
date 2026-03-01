fd4e78336f2870aad12bd3889c01486b8bce0792
ABDULRAHMAN BAWAZIR <129544288+Abdulrahman-Bawazir@users.noreply.github.com>
2026-03-01 07:23:06 +0300

    Update hotel revenue.sql

--- hotel revenue.sql
+++ hotel revenue.sql
@@ -61,3 +61,67 @@ select h.lead_time ,h.stays_in_week_nights, h.hotel,t.lead_time ,t.stays_in_week
 FROM early_year as h
 left join one_year  as t
 on h.hotel=t.hotel;
+
+WITH hotels AS (
+    SELECT * FROM dbo.[2018]
+    UNION ALL
+    SELECT * FROM dbo.[2019]
+    UNION ALL
+    SELECT * FROM dbo.[2020]
+)
+
+SELECT 
+    arrival_date_year,
+    SUM((stays_in_week_nights + stays_in_weekend_nights) * adr) AS Revenue,
+    LAG(SUM((stays_in_week_nights + stays_in_weekend_nights) * adr))
+        OVER (ORDER BY arrival_date_year) AS Previous_Year_Revenue,
+    ROUND(
+        (SUM((stays_in_week_nights + stays_in_weekend_nights) * adr) -
+        LAG(SUM((stays_in_week_nights + stays_in_weekend_nights) * adr))
+        OVER (ORDER BY arrival_
+ date_year))
+        * 100.0 /
+        LAG(SUM((stays_in_week_nights + stays_in_weekend_nights) * adr))
+        OVER (ORDER BY arrival_date_year)
+    ,2) AS Growth_Percentage
+FROM hotels
+GROUP BY arrival_date_year
+ORDER BY arrival_date_year;
+
+--Cancellation Rate by Year--
+
+WITH hotels AS (
+    SELECT * FROM dbo.[2018]
+    UNION ALL
+    SELECT * FROM dbo.[2019]
+    UNION ALL
+    SELECT * FROM dbo.[2020]
+)
+
+SELECT 
+    arrival_date_year,
+    COUNT(*) AS Total_Bookings,
+    SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS Total_Cancellations,
+    ROUND(
+        SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*)
+    ,2) AS Cancellation_Rate_Percentage
+FROM hotels
+GROUP BY arrival_date_year;
+
+--Revenue by Market Segment (Joined Tables)--
+WITH hotels AS (
+    SELECT * FROM dbo.[2018]
+    UNION ALL
+    SELECT * FROM dbo.[2019]
+    UNION ALL
+    SELECT * FROM dbo.[2020]
+)
+
+SELECT 
+    h.market_segment,
+    SUM((h.stays_in_week_nights + h.stays_in_weekend_nights) * h.adr) AS Revenue,
+    COUNT(*) AS Total_Bookings
+FROM hotels h
+GROUP BY h.market_segment
+ORDER BY Revenue DESC;
+
